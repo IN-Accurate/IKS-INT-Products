0a67f82a68dbd22b1c29457ffb5643dbdd200ca3
Anbarasu <anbarasun123@gmail.com>
2023-01-05 14:00:39 +0530

    refactor: home

--- iks-product-showcase/src/App.js
+++ iks-product-showcase/src/App.js
@@ -1,17 +1,17 @@
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import Home from './components/Home/Home'
-import About from './components/About/About'
-import Product from './components/Product/Product'
-import Footer from './components/Footer/Footer'
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Product from "./components/Product/Product";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <Home/>
-     <About/>
-     <Product/>
-     <Footer/>
+      <Navbar />
+      <Home />
+      <About />
+      <Product />
+      <Footer />
     </div>
   );
 }
