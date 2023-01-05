03603c3c8289b4c70791577e8acc0205e126ff5b
notS0LD13R <joelsebin@gmail.com>
2023-01-05 22:02:38 +0530

    Adding basic structure and style to products section

--- iks-product-showcase/src/components/Product/ProductBox.jsx
+++ iks-product-showcase/src/components/Product/ProductBox.jsx
@@ -1,11 +1,41 @@
 // import { useRef } from 'react';
+import { useState } from 'react'
 import './ProductBox.css'
 
-function ProductBox() {
+function ProductBox(props) {
+    const text=props.text
+    const [toggle,settoggle]=useState(false)
+    const bookmark=[
+        "/Media/book_hollow.svg",
+        "/Media/book_solid.svg"
+    ]
 
-  return (
+
+    return (
     <div className="product-box">
         <img src="https://steeinstad.nl/wp-content/uploads/2017/08/d2-placeholder-1080px.png" alt="Product IMG" />
+        <h2>{text.title}</h2>
+        <div className='card-content'>
+            <div class="flex">
+            <p> {text.para} </p>
+            <div className="icon-container">
+                <img src={toggle ? bookmark[0] : bookmark[1]} alt="" onClick={()=>settoggle(!toggle)}/>
+            </div>
+            </div>
+            
+            <div className="button-container flex">
+                <button type="button" class="slide">
+                    <div>READMORE</div>
+                    <i class="icon-arrow-right">{'>'}</i>
+                </button>
+                <button type="button" class="slide">
+                    <div>LIKE</div>
+                    <i class="icon-arrow-right">❤</i>
+                </button>
+                
+            </div>
+            
+        </div>
     </div>
   )
 }
