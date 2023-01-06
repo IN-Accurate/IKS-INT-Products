baf7244eb61b096b226f447b20e52cc2d4e6062a
Anbarasu <anbarasun123@gmail.com>
2023-01-06 10:31:55 +0530

    Fix/product (#13)
    * refactor: css
    
    * chore: prettify

--- iks-product-showcase/src/components/Product/Product.jsx
+++ iks-product-showcase/src/components/Product/Product.jsx
@@ -1,29 +1,56 @@
-import ProductBox  from './ProductBox'
-import './Product.css'
-
+import ProductBox from "./ProductBox";
+import "./Product.css";
 
 function Product() {
-  const BoxContent=[
-    {title:'INKED',shorttext:"Lorem ipsum dolor",longtext:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, rem."},
-    {title:'Min T',shorttext:"Lorem ipsum dolor",longtext:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, rem."},
-    {title:'Mint Bucket',shorttext:"Lorem ipsum dolor",longtext:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, rem."},
-    {title:'MinT forms',shorttext:"Lorem ipsum dolor",longtext:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, rem."},
-    {title:'Mimo',shorttext:"Lorem ipsum dolor",longtext:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, rem."},
-    {title:'min.Ic',shorttext:"Lorem ipsum dolor",longtext:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, rem."},
-  ]
+  const BoxContent = [
+    {
+      title: "INKED",
+      shorttext: "Lorem ipsum dolor",
+      longtext:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, rem.",
+    },
+    {
+      title: "Min T",
+      shorttext: "Lorem ipsum dolor",
+      longtext:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, rem.",
+    },
+    {
+      title: "Mint Bucket",
+      shorttext: "Lorem ipsum dolor",
+      longtext:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, rem.",
+    },
+    {
+      title: "MinT forms",
+      shorttext: "Lorem ipsum dolor",
+      longtext:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, rem.",
+    },
+    {
+      title: "Mimo",
+      shorttext: "Lorem ipsum dolor",
+      longtext:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, rem.",
+    },
+    {
+      title: "min.Ic",
+      shorttext: "Lorem ipsum dolor",
+      longtext:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, rem.",
+    },
+  ];
 
-  const BoxComp=BoxContent.map((dict,key)=>{
-    return <ProductBox text={dict} key={key}/>
-  })
-
-  
   return (
     <div className="product-container" id="product-container">
       <h1>FEATURED</h1>
-      {BoxComp}
-      
+      <div className="product-grid">
+        {BoxContent.map((dict, key) => (
+          <ProductBox text={dict} key={key} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
