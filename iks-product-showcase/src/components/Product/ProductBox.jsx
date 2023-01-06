b3d6d2e38786163df333ddda54a80082fe5a8c8b
notS0LD13R <joelsebin@gmail.com>
2023-01-07 00:07:20 +0530

    Added hover zoom on images

--- iks-product-showcase/src/components/Product/ProductBox.jsx
+++ iks-product-showcase/src/components/Product/ProductBox.jsx
@@ -6,10 +6,11 @@ function ProductBox(props) {
   const navigate = useNavigate();
   return (
     <div className="product-box">
-      <img
+      <div className="image-container"><img
         src={text.image || "https://via.placeholder.com/400x300"}
         alt="Product IMG"
-      />
+      /></div>
+      
       <h2>{text.name}</h2>
       <div className="card-content">
         <div className="flex">
