83499d2a4c1c6c768b0cf0cbde4130bc35c3b45c
Anbarasu <anbarasun123@gmail.com>
2023-01-06 10:31:03 +0530

    chore: prettify

--- iks-product-showcase/src/components/About/About.js
+++ iks-product-showcase/src/components/About/About.js
@@ -1,15 +1,27 @@
-import './About.css'
+import "./About.css";
 function About() {
   return (
     <div className="about-container" id="about-container">
-      <div className="about-header">
-      ABOUT
-      </div>
+      <div className="about-header">ABOUT</div>
       <div className="about-content">
-      The IKS Interns Team was born with the introduction of a new initiative by IEEE Kerala Section to select a group of resourceful interns to its Electronic Communications Coordination team. As a result, a team of sixteen interns was formed, headed by Ms. Sarada Jayakrishnan, Chair, IEEE Kerala Section, and handled by Mr. Alan Mathew, ECC, IEEE Kerala Section. Divided into three groups - Web Team, Design Team, and Content Team - the Interns Team consists of students from the different Student Branches of the Section. Conceived from an idea aimed at training and mentoring a selected number of students and transforming them into valuable volunteers determined to work for the betterment of the Section, the Interns Team has considerably grown to handle tasks within a professional network. In a span of seven months, the team has been successful in completing various projects and Section related activities, showcasing enhanced skillset, attained as a result of brilliant mentorship and rigorous team management.
+        The IKS Interns Team was born with the introduction of a new initiative
+        by IEEE Kerala Section to select a group of resourceful interns to its
+        Electronic Communications Coordination team. As a result, a team of
+        sixteen interns was formed, headed by Ms. Sarada Jayakrishnan, Chair,
+        IEEE Kerala Section, and handled by Mr. Alan Mathew, ECC, IEEE Kerala
+        Section. Divided into three groups - Web Team, Design Team, and Content
+        Team - the Interns Team consists of students from the different Student
+        Branches of the Section. Conceived from an idea aimed at training and
+        mentoring a selected number of students and transforming them into
+        valuable volunteers determined to work for the betterment of the
+        Section, the Interns Team has considerably grown to handle tasks within
+        a professional network. In a span of seven months, the team has been
+        successful in completing various projects and Section related
+        activities, showcasing enhanced skillset, attained as a result of
+        brilliant mentorship and rigorous team management.
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
