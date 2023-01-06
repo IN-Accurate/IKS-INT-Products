9dd2558d510d1ab645d582514e817f63ca8b498d
IN-Accurate <vishalsankar410@gmail.com>
2023-01-06 17:32:15 +0530

    fix product and read more function

--- iks-product-showcase/src/Routes/Product.js
+++ iks-product-showcase/src/Routes/Product.js
@@ -3,9 +3,9 @@ import { useParams } from "react-router-dom";
 export const ProductPage = () => {
   const { productId } = useParams();
   return (
-    <div className="App">
-      <h1>Product</h1>
-      <p>Product ID: {productId}</p>
+    <div className="routes-product-container">
+      <h1 className="header-products">Product</h1>
+      <p className="product-id-container">Product ID: {productId}</p>
     </div>
   );
 };
--- iks-product-showcase/src/components/Product/ProductBox.jsx
+++ iks-product-showcase/src/components/Product/ProductBox.jsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import "./ProductBox.css";
 
 function ProductBox(props) {
   const { text } = props;
-
+  const [isExpanded, setIsExpanded] = useState(false);
   return (
     <div className="product-box">
       <img
@@ -11,10 +12,28 @@ function ProductBox(props) {
       />
       <h2>{text.title}</h2>
       <div className="card-content">
+        {!isExpanded && 
+        <div className="flex">
+          <p> {text.shorttext} </p>
+          <button
+            onClick={() => {
+              setIsExpanded(!isExpanded);
+            }}
+          >
+            Know More
+          </button>
+        </div>}
+        {isExpanded && 
         <div className="flex">
           <p> {text.longtext} </p>
-          <button>Know More</button>
-        </div>
+          <button
+            onClick={() => {
+              setIsExpanded(!isExpanded);
+            }}
+          >
+            Show Less
+          </button>
+        </div>}
       </div>
     </div>
   );
